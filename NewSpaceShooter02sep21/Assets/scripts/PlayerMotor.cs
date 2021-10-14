3374370d8ba15fc60e2e74c20ebca4655ec0f7a7
DavidSJones <Jones_David_S@yahoo.com>
2021-10-14 14:39:57 -0500

    Update PlayerMotor.cs

--- NewSpaceShooter02sep21/Assets/scripts/PlayerMotor.cs
+++ NewSpaceShooter02sep21/Assets/scripts/PlayerMotor.cs
@@ -26,6 +26,9 @@ public class PlayerMotor : MonoBehaviour
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> parent of 9691f7b (ForwardSpeedStuff00)
+=======
 >>>>>>> parent of 9691f7b (ForwardSpeedStuff00)
 =======
 >>>>>>> parent of 9691f7b (ForwardSpeedStuff00)
